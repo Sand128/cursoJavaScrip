15ed1ef9638788679043273ed2f83cd1c0b94c45
Sand128 <131736085+Sand128@users.noreply.github.com>
2024-01-31 21:34:30 -0600

    actualiza app.js
    Se hace uso de clases, para mostrar un mensaje en el parrafo del html

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
 let titulo= document.querySelector('h1');
 titulo.innerHTML='Juego del número secreto'
+
+
+let parrafo = document.querySelector('p');
+parrafo.innerHTML = 'Indica un número del 1 al 10';
